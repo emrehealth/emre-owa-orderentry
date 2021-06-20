33a24932a1cb39bc849fa9334e1f83f1d8fd2a10
Danial Betres <danialbetres@Danials-MacBook-Air.local>
2021-06-20 15:09:04 -0400

    Fix formatting issues

--- app/js/components/orderEntry/DrugOrderDetails.jsx
+++ app/js/components/orderEntry/DrugOrderDetails.jsx
@@ -5,34 +5,34 @@ import { FormattedMessage } from "react-intl";
 const DrugOrderDetails = ({
   details, activeDates, orderer,
 }) => {
-  /* TODO: The order object doesn't contain the details of the drug seperated into each respective field.
-   Everything is grouped in the "display" field. We should update the contents of the order object to 
-   maintain the original fields so that we can display that here versus "display". */
-  details = details.replace("(NEW)", "")
+  /* TODO: The order object doesn't contain the details of the drug seperated into each respective
+   field. Everything is grouped in the "display" field. We should update the contents of the order
+   object to maintain the original fields so that we can display that here versus "display". */
+  const formattedDetails = details.replace("(NEW)", "")
 
   return (
-  <div className="order-details">
-    <div className="details">
-      <ul className="details-section">
-        <b className="order-details-titles">
-          <FormattedMessage
-            id="app.orders.details"
-            defaultMessage="Details" />:
-        </b> {details}
-      </ul>
-      <ul className="details-section">
-        <b className="order-details-titles"> 
-          <FormattedMessage
-            id="app.orders.dateAdministered"
-            defaultMessage="Date Administered" />:
-        </b> {activeDates}
-      </ul>
-      <p><FormattedMessage
-        id="app.orders.orderedBy"
-        defaultMessage="Ordered by" /> {orderer}
-      </p>
+    <div className="order-details">
+      <div className="details">
+        <ul className="details-section">
+          <b className="order-details-titles">
+            <FormattedMessage
+              id="app.orders.details"
+              defaultMessage="Details" />:
+          </b> {formattedDetails}
+        </ul>
+        <ul className="details-section">
+          <b className="order-details-titles"> 
+            <FormattedMessage
+              id="app.orders.dateAdministered"
+              defaultMessage="Date Administered" />:
+          </b> {activeDates}
+        </ul>d
+        <p><FormattedMessage
+          id="app.orders.orderedBy"
+          defaultMessage="Ordered by" /> {orderer}
+        </p>
+      </div>
     </div>
-  </div>
   )
 }
 
